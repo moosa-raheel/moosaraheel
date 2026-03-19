c4685d86cb673e1be812bc90bf4963d37893bbf0
moosa-raheel <moosarahil13@gmail.com>
2026-03-19 07:12:50 +0500

    Setup domain, email, and basic SEO

---
+++ src/app/sitemap.js
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://moosaraheel.com", priority: 1 },
+    { url: "https://moosaraheel.com/about" },
+    { url: "https://moosaraheel.com/projects" },
+    { url: "https://moosaraheel.com/blog" },
+  ];
+}
